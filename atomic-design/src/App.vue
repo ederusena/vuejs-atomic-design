346db9ac62992a9de1275813899a45b4f42503f8
Eder Sena <eder.sena@live.com>
2022-01-08 21:30:16 -0300

    Dale

---
+++ atomic-design/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="component">
+    <logo />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Logo } from '@/components/atoms'
+
+export default defineComponent({
+  components: {
+    Logo
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.component {
+  width: 300px;
+}
+</style>
